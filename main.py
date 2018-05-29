4eff06da50cb20721b668acaf951e99012f0b984
Luis Fernando Contreras Guerra <luisguerra495@gmail.com>
2018-05-29 10:01:12 -0500

    Añadido el manejador de errores

--- main.py
+++ main.py
@@ -4,15 +4,19 @@ numero_azar = randint(0,20)
 
 print("Hola, bienvenido a adivina el numero!\n")
 
-for i in range(5):
-	estimacion = int(input("\nIngresa una estimación: "))
+try:
+	for i in range(5):
+		estimacion = int(input("\nIngresa una estimación: "))
 
-	if estimacion == numero_azar:
-		print("Has ganado el juego en {0} intento".format(i + 1))
-		exit()
-	elif estimacion > numero_azar:
-		print("El numero estimado es muy grande!")
-	else:
-		print("El numero estimado es muy pequeño!")
+		if estimacion == numero_azar:
+			print("Has ganado el juego en {0} intento".format(i + 1))
+			exit()
+		elif estimacion > numero_azar:
+			print("El numero estimado es muy grande!")
+		else:
+			print("El numero estimado es muy pequeño!")
+
+except Exception:
+	print("\nLa entrada de datos no es la correcta!!!")
 
 print("\nLo siento, has perdido el juego :(")
